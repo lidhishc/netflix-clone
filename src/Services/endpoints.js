bfe93370d98d0a2886fff1cb4f4fae76e3656e60
Lidhish C <37290709+lidhishc@users.noreply.github.com>
2020-12-06 22:49:46 +0530

    Update endpoints.js

--- src/Services/endpoints.js
+++ src/Services/endpoints.js
@@ -1,14 +1,14 @@
-const TMBD_API_V3 = "2097fbfc8bc64540593a3ef22c213ba6"
+const API_V3 = "2097fbfc8bc64540593a3ef22c213ba6"
 
 
 const requests = {
-    fetchActionMovies: `/discover/movie?api_key=${TMBD_API_V3}&with_genres=28`,
-    fetchComedyMovies: `/discover/movie?api_key=${TMBD_API_V3}&with_genres=35`,
-    fetchHorrorMovies: `/discover/movie?api_key=${TMBD_API_V3}&with_genres=27`,
-    fetchRomanceMovies: `/discover/movie?api_key=${TMBD_API_V3}&with_genres=10749`,
-    fetchDocumentaries: `/discover/movie?api_key=${TMBD_API_V3}&with_genres=99`,
-    fetchTrending: `/trending/movie/week?api_key=${TMBD_API_V3}&language=en-US`,
-    fetchNetflixOriginals: `/discover/tv?api_key=${TMBD_API_V3}&with_networks=213`,
+    fetchActionMovies: `/discover/movie?api_key=${API_V3}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_V3}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_V3}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_V3}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_V3}&with_genres=99`,
+    fetchTrending: `/trending/movie/week?api_key=${API_V3}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_V3}&with_networks=213`,
 }
 
 export default requests; 
